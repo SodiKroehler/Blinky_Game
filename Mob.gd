351e88c5956f45fbe9a43fcffc8eec49a5500958
Sodi Kroehler <sodikroehler@outlook.com>
2020-10-23 19:56:16 -0400

    7.55PM commit - introductory files

---
+++ Mob.gd
@@ -0,0 +1,15 @@
+extends RigidBody2D
+
+export var min_speed = 150
+export var max_speed = 250
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$AnimatedSprite.animation = "walk"
+
+func _on_VisibilityNotifier2D_screen_exited():
+	queue_free()
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
